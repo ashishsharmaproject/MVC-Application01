72885d6928fa5060860418123910cf5509e63632
ashishsharmaproject <mail.ashishsharma53@gmail.com>
2023-10-25 01:49:43 +0530

    Add project files.

---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace MVC_Application01.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        SqlConnection con = new SqlConnection("data source=NEHAASHISHARMA;initial catalog=MVC_Application_DB;integrated security=true");
+        // GET: UserRegistration
+        public ActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public void insert_oparation(string A, string B, int C)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("user_insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", A);
+            cmd.Parameters.AddWithValue("@city", B);
+            cmd.Parameters.AddWithValue("@age", C);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        public JsonResult Display_oparation()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("user_show", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            string data = JsonConvert.SerializeObject(dt);
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
